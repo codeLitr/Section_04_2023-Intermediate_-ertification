08f42735603291e47f7c8e77a6dcb1bf63b37afb
SMV <kraft088@gmail.com>
2023-04-28 01:03:27 +1100

    Add HomeWork

---
+++ !Intermediate_Certification/HomeTask_02/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 66:
+// Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputDigit(string message) // Ввод чисел M и N.
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void CheckNaturalDigitAndPrintResult(int m, int n) // Проверка введенных чисел (натуральное или нет) и вывод результата.
+{
+    if (m <= 0 || n <= 0)
+    {
+        System.Console.Write("Ошибка ввода - ненатуральное число.");
+        return;
+    }
+    else
+    {
+        System.Console.Write($"M = {m}; N = {n} -> {SumInterval(m, n)}");
+    }
+}
+// m = 2, n = 3 -> A(m,n) = 9
+
+int SumInterval(int m, int n) // Суммирование чисел в интервале от M до N и от N до M, если M > N.
+{
+    int sum = 0;
+    if (m < n)
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+    else
+    {
+        for (int i = n; i <= m; i++)
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int m = InputDigit("Введите число M: ");
+int n = InputDigit("Введите число N: ");
+
+CheckNaturalDigitAndPrintResult(m, n);
+SumInterval(m, n);
---
+++ !Intermediate_Certification/HomeTask_03/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 68:
+// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputDigit(string message) // Ввод чисел m и n.
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void CheckNonNegativeAndPrintResult(int m, int n) // Проверка введенных чисел (отрицательное или нет) и вывод результата.
+{
+    if (m < 0 || n < 0)
+    {
+        System.Console.Write("Ошибка ввода - отрицательное число.");
+        return;
+    }
+    else
+    {
+        System.Console.Write($"m = {m}, n = {n} -> A(m,n) = {MethodAckermann(m, n)}");
+    }
+}
+
+int MethodAckermann(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return MethodAckermann(m - 1, 1);
+    }
+    else
+    {
+        return MethodAckermann(m - 1, MethodAckermann(m, n - 1));
+    }
+}
+
+int m = InputDigit("Введите число N: ");
+int n = InputDigit("Введите число N: ");
+
+CheckNonNegativeAndPrintResult(m, n);
+MethodAckermann(m, n);
